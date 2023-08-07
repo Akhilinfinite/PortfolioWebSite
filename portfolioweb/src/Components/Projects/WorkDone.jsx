4256ecf67b950ca815977f5f18684d5d5822e7ec
Akhilinfinite <116935898+Akhilinfinite@users.noreply.github.com>
2023-08-07 15:23:50 +0530

    final draft

--- portfolioweb/src/Components/Projects/WorkDone.jsx
+++ portfolioweb/src/Components/Projects/WorkDone.jsx
@@ -11,6 +11,24 @@ export default class WorkDone extends Component {
             Some of the noteworth project I have done
           </div>
         </div>
+        <div className="projects">
+          <div className="Project1">
+            <div className="video">
+              <video width="400" height="225" autoPlay muted controls >
+                <source src="/ProjectFolder/Demo.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+            <div className="content">
+              <div className="ProjectTitle">Deccan Pavillion</div>
+              <div className="ProjectDescrption">
+                It is an food ordering website. It is for a single restaurent.
+                where you can login to the site with excisting cradentials. And
+                see the Items have in the restaurent and add them to the cart.
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
